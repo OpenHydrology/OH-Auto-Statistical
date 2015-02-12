224cf216651854d4575d22221f8a53e5cd229181
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-02-12 21:49:34 +0000

    version file

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from setuptools import setup
+from os import path
 
+here = path.abspath(path.dirname(__file__))
+version = open(path.join(here, 'VERSION')).read()
 
 setup(
     name='autostatistical',
-    version='0.4.1',
+    version=version,
     packages=['autostatistical'],
     install_requires=[
         'Jinja2>=2.7,<2.8',
