42ff9a87fc98743a8afa61effb8533c013bdda9f
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-02-13 21:22:11 +0000

    build 0 version fix

--- version.py
+++ version.py
@@ -11,14 +11,21 @@ def update():
                     'describe',
                     '--tags',
                     '--always']
-        full_version = check_output(git_args, universal_newlines=True)
-        tag, number = full_version.split('-')[0:2]
+        full_version = check_output(git_args, universal_newlines=True).split('-')
+
+        tag = full_version[0]
         if not tag.startswith(TAG_PREFIX):
             raise ValueError("Tag `{}` must start with `{}`.".format(tag, TAG_PREFIX))
-        version = tag[len(TAG_PREFIX):]
+        version = tag[len(TAG_PREFIX):].strip()
+
+        if len(full_version) == 1:
+            version_str = version
+        else:
+            number = full_version[1].strip()
+            version_str = '-'.join([version, number])
 
         with open('VERSION', mode='w', encoding='utf-8') as version_file:
-            version_file.write('-'.join([version, number]))
+            version_file.write(version_str)
 
     except CalledProcessError:
         return
