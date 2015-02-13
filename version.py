aef6f8a1aa877119f575d982c154b41f416c6efc
Florenz A. P. Hollebrandse <faph@users.noreply.github.com>
2015-02-13 06:53:58 +0000

    check valid tag and correct full version string

--- version.py
+++ version.py
@@ -13,10 +13,12 @@ def update():
                     '--always']
         full_version = check_output(git_args, universal_newlines=True)
         tag, number = full_version.split('-')[0:2]
-        tag = tag[len(TAG_PREFIX):]
+        if not tag.startswith(TAG_PREFIX):
+            raise ValueError("Tag `{}` must start with `{}`.".format(tag, TAG_PREFIX))
+        version = tag[len(TAG_PREFIX):]
 
         with open('VERSION', mode='w', encoding='utf-8') as version_file:
-            version_file.write('{}-{}'.format(tag, number))
+            version_file.write('-'.join([version, number]))
 
     except CalledProcessError:
         return
